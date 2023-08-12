2d4964712092fca5b2e1ac1cc61e76489be66648
Fullpacki <113352776+Fullpacki@users.noreply.github.com>
2023-08-12 02:00:42 +0200

    end

--- social-proof/src/components/Rates.js
+++ social-proof/src/components/Rates.js
@@ -2,7 +2,7 @@ import IconStar from "../images/icon-star.svg";
 
 function Rates() {
   return (
-    <div className=" flex flex-col gap-4 rounded-xl xl:w-2/3 xl:items-center xl:justify-center xl:py-10">
+    <div className=" flex flex-col gap-4 rounded-xl xl:w-2/3 xl:items-center  xl:py-10 border">
       <div className="flex flex-col justify-center items-center gap-3 py-3 rounded-xl bg-[#f7f2f8] xl:flex-row xl:gap-6 xl:w-2/3 xl:py-4">
         <div className="flex flex-row gap-4">
           <img src={IconStar} alt="A star icon who's rating reviews" />
@@ -27,7 +27,7 @@ function Rates() {
           Rated 5 stars in Report Guru
         </p>
       </div>
-      <div className="flex flex-col justify-center items-center gap-3 py-3 rounded-xl bg-[#f7f2f8] xl:flex-row xl:gap-6 xl:w-2/3 xl:py-4 xl:translate-x-[10em]">
+      <div className="flex flex-col justify-center items-center gap-3 py-3 rounded-xl bg-[#f7f2f8] xl:flex-row xl:gap-6 xl:w-2/3 xl:py-4 xl:translate-x-[9em]">
         <div className="flex flex-row gap-4">
           <img src={IconStar} alt="A star icon who's rating reviews" />
           <img src={IconStar} alt="A star icon who's rating reviews" />
