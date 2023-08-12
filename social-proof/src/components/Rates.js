b92f130d2de31bbab61a35fcd7f12b3a2ea2392b
Fullpacki <113352776+Fullpacki@users.noreply.github.com>
2023-08-12 02:04:07 +0200

    -border

--- social-proof/src/components/Rates.js
+++ social-proof/src/components/Rates.js
@@ -2,7 +2,7 @@ import IconStar from "../images/icon-star.svg";
 
 function Rates() {
   return (
-    <div className=" flex flex-col gap-4 rounded-xl xl:w-2/3 xl:items-center  xl:py-10 border">
+    <div className=" flex flex-col gap-4 rounded-xl xl:w-2/3 xl:items-center  xl:py-10">
       <div className="flex flex-col justify-center items-center gap-3 py-3 rounded-xl bg-[#f7f2f8] xl:flex-row xl:gap-6 xl:w-2/3 xl:py-4">
         <div className="flex flex-row gap-4">
           <img src={IconStar} alt="A star icon who's rating reviews" />
